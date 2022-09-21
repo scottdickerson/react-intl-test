c9127dd2c30c63af54a188d27c6e312d824acfcc
Scott Dickerson <scott.dickerson@contrastsecurity.com>
2022-09-21 10:18:14 -0500

    feat(typescript): error on children

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { FormattedDate, IntlProvider } from "react-intl";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IntlProvider locale="ja">
+      <div className="App">
+        <header className="App-header">
+          <FormattedDate value={new Date()}>
+            {(value: string) => <strong>{value}</strong>}
+          </FormattedDate>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </IntlProvider>
   );
 }
 
